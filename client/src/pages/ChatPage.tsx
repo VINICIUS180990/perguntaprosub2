9d618412988b2ff2cdb9a263fa3279ef0c9a660f
VINICIUS180990 <vglempresarial@gmail.com>
2025-06-21 20:24:14 -0300

    Salvando progresso inicial

---
+++ client/src/pages/ChatPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ChatPage: React.FC = () => {
+  return (
+    <div className="chat-page-container">
+      <h1>Chat</h1>
+      {/* Adicione aqui a interface do chat */}
+      <div className="chat-window">
+        <p>Bem-vindo ao chat!</p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatPage;
